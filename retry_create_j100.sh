f18757dac0c1b906dacb5eab6f159ce979588061
Brendan O'Connor <brendan@soundpatrol.com>
2026-01-28 23:43:16 +0000

    adds instance creation script with auto retries

---
+++ retry_create_j100.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# save as: retry_create_h100.sh
+
+INSTANCE_NAME="brens-8h100s"
+ZONE="us-central1-a"
+RETRY_INTERVAL=300  # 5 minutes between retries
+
+while true; do
+  echo "[$(date)] Attempting to create $INSTANCE_NAME..."
+  
+  gcloud compute instances create "$INSTANCE_NAME" \
+    --zone="$ZONE" \
+    --machine-type=a3-highgpu-8g \
+    --boot-disk-size=500GB \
+    --boot-disk-type=pd-ssd \
+    --source-snapshot=brens-system-snapshot \
+    --disk=name=brens-data-disk-15tb,mode=rw \
+    --maintenance-policy=TERMINATE \
+    --restart-on-failure 2>&1
+  
+  if [ $? -eq 0 ]; then
+    echo "[$(date)] SUCCESS! Instance $INSTANCE_NAME created."
+    # Optional: send yourself an email/notification
+    # echo "H100 VM created" | mail -s "GCP VM Ready" your@email.com
+    break
+  else
+    echo "[$(date)] Failed. Retrying in $RETRY_INTERVAL seconds..."
+    sleep $RETRY_INTERVAL
+  fi
+done
